d03c6c0eba3447af1e916236ac629c3e31b45bc0
Eirik Vullum <evullum@gmail.com>
2014-05-19 10:48:27 +0200

    refactor for multiple instances

--- example/test.js
+++ example/test.js
@@ -17,13 +17,17 @@ var getFeeds = function (options, callback) {
     callback(null, feeds.filter(isActive));
 };
 
+// RssReader
+var RssReader = require('../lib')();
+
 // rss reader/scraper module
-var rssReader = require('../lib')({
+var rssReader = new RssReader({
     getSources: getFeeds,
     handleEntry: handleEntry,
     ipc: ipc,
     sockets: 15,
-    waitTime: 10000
+    waitTime: 10000,
+    timeOut: 5000
 });
 
 console.log('running rss-reader');
