db8d47da63f13b0c2ca8074e2af9882921721ee3
Eirik Vullum <evullum@gmail.com>
2014-08-17 13:57:27 +0200

    add another test template

--- example/template.js
+++ example/template.js
@@ -1,5 +1,66 @@
 exports = module.exports = [
     {
+        active: true,
+        origin: 'feed',
+        name: 'TheWireCutter',
+        url: 'http://feeds.feedburner.com/TheWirecutter',
+        linkref: 'url',
+        category: ['technology' ,'thewirecutter'],
+        format: 'desktop',
+        body: true,
+        template: {
+            elements: [
+                {
+                    name: 'guid',
+                    type: 'url',
+                    required: true,
+                    items: [
+                        {
+                            selector: 'guid'
+                        },
+                        {
+                            selector: 'link'
+                        },
+                        {
+                            selector: 'title',
+                            decode: true
+                        }
+                    ]
+                },
+                {
+                    name: 'title',
+                    required: true,
+                    items: [
+                        {
+                            selector: 'title',
+                            decode: true
+                        }
+                    ]
+                },
+                {
+                    name: 'url',
+                    type: 'url',
+                    required: true,
+                    items: [
+                        {
+                            selector: 'link'
+                        }
+                    ]
+                },
+                {
+                    name: 'image',
+                    type: 'url',
+                    items: [
+                        {
+                            selector: 'enclosures[0].url'
+                        }
+                    ],
+                    fallback: 'http://thewirecutter.wpengine.netdna-cdn.com/wp-content/themes/thewirecutter/thewirecutter/img/logo/header.png'
+                }
+            ]
+        }
+    },
+    {
         active: false,
         origin: 'feed',
         name: 'Gamer.no',
@@ -29,7 +90,6 @@ exports = module.exports = [
                 },
                 {
                     name: 'title',
-
                     required: true,
                     items: [
                         {
@@ -183,7 +243,7 @@ exports = module.exports = [
         }
     },
     {
-        active: true,
+        active: false,
         origin: 'feed',
         name: 'TechCrunch',
         url: 'http://feeds.feedburner.com/techCrunch',
